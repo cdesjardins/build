a056b25de6e9a7264c6a8c51e71b438a4737dd95
Chris Desjardins <git@chrisd.info>
2015-09-23 18:04:16 +0200

    Add installed size to deb

--- makedeb.sh
+++ makedeb.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/bin/bash -e
 
 VERSION=`(cd ../ComBomb && git describe) | sed s/v//`
 PACKAGE=ComBomb-$VERSION
 INSTDIR=usr
 rm -rf build/ComBomb/ComBomb build/ComBomb/$PACKAGE *.deb
 tar jxvf build/ComBomb/ComBomb-*.tar.bz2 -C build/ComBomb/
+INSTSIZE=`du --block-size 1024 -c build/ComBomb/ComBomb | grep "	total" | cut -d "	" -f 1`
 
 mkdir -p build/ComBomb/$PACKAGE/$INSTDIR/share/applications
 mkdir -p build/ComBomb/$PACKAGE/$INSTDIR/share/pixmaps
@@ -18,6 +19,7 @@ echo "Priority: optional" >> build/ComBomb/$PACKAGE/DEBIAN/control
 echo "Architecture: amd64" >> build/ComBomb/$PACKAGE/DEBIAN/control
 #echo "Depends: libsomethingorrather (>= 1.2.13), anotherDependency (>= 1.2.6)" >> build/ComBomb/DEBIAN/control
 echo "Maintainer: Chris Desjardins <cjd@chrisd.info>" >> build/ComBomb/$PACKAGE/DEBIAN/control
+echo "Installed-Size: $INSTSIZE" >> build/ComBomb/$PACKAGE/DEBIAN/control
 echo "Description: ComBomb" >> build/ComBomb/$PACKAGE/DEBIAN/control
 echo " The turbo encabulator of terminal emulators" >> build/ComBomb/$PACKAGE/DEBIAN/control
 
